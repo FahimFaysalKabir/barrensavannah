a13a255aa97443d3bf0764e9dd90145ab08782b0
Fahim Faysal Kabir <githubbadhon@gmail.com>
2023-08-22 17:36:58 +0600

    done
    22.8.2023

--- index.php
+++ index.php
@@ -13,51 +13,35 @@ Released   : 20081009
 
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html <?php language_attributes(); ?>>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="barrensavannah.css" />
-<title>Barren Savannah by Bryant Smith</title>
+<meta charset="<?php bloginfo('charset'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/barrensavannah.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css" />
+<title><?php bloginfo('title');?></title>
+<?php wp_head();?>
 </head>
 
-<body>
+<body <?php body_class();?>>
     <div id="page">
-        <div class="topNaviagationLink"><a href="index.html">Home</a></div>
-        <div class="topNaviagationLink"><a href="index.html">About</a></div>
-        <div class="topNaviagationLink"><a href="index.html">Portfolio</a></div>
-        <div class="topNaviagationLink"><a href="index.html">Services</a></div>
-	    <div class="topNaviagationLink"><a href="index.html">Contact</a></div>
+    <?php wp_nav_menu('um')?>
 	</div>
     <div id="mainPicture">
-    	<div class="picture">
-        	<div id="headerTitle">Barren Savannah</div>
-            <div id="headerSubtext">An XHTML 1.0 Strict Template by Bryant Smith</div>
+    	<div class="picture" style="background-image:url(<?php header_image();?>)">
+        	<div id="headerTitle"><?php bloginfo('title');?></div>
+            <div id="headerSubtext"><?php bloginfo('description');?></div>
         </div>
     </div>
         <div class="contentBox">
     	<div class="innerBox">
-        	<h1>The Title of an Article</h1>
+      <?php while(have_posts()):the_post(); ?>
+        	<h1><?php the_title(); ?></h1>
           <div class="contentText">
-            <p>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer.   This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :) <a href="#">This is what a link looks like.</a></p>
-            <br />
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</p><br />
-
-                <blockquote><p>This is a block quote, use it to include quotes from yourself or others.  All you have to do to include this element is wrap some text around blockquote tags</p></blockquote>
+             <?php the_content(); ?>
                 </div>
 
+                <?php endwhile; ?>
         
-        <h1>Titles are H1 Tags</h1>
-            <div class="contentText"><p> Since the titles are H1 tags, try to include your keywords in them as search engines will look at them as being important content.</p><br />
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</p><br />
-        
-          <p>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</p></div>
-        
-        
-        <h1>Yet Another One!</h1>
-          <div class="contentText"><p>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer.   This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :) </p><br />
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</p><br />
-          <p>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</p></div>
-          
           
           <!-- Please leave this in place after all of your content - thanks :) -->
 <div id="footer"><a href="http://www.aszx.net">web development</a> by <a href="http://www.bryantsmith.com">bryant smith</a></div>
@@ -75,6 +59,6 @@ Released   : 20081009
 
 
     </div>
-        
+    <?php wp_footer(); ?>       
 </body>
 </html>
